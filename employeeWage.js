b19c812e91d910320e3d864ccf10a54ba0c22a86
faizanpro77 <faizyshaikh204@gmail.com>
2021-07-02 00:02:55 +0530

    MODIFIED : add functionality to calculate wage for month

--- employeeWage.js
+++ employeeWage.js
@@ -1,10 +1,11 @@
-//use function to calculate wage
+// calculate employee wage for month
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURSE = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20; 
+NUMBER_OF_WORKING_DAYS = 20;
 
 function getWorkingHourse(empCheck) {
 switch (empCheck) {
@@ -20,7 +21,9 @@ switch (empCheck) {
 }
 
 let empHrs = 0;
+for (day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
 let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHourse(empCheck)
+empHrs += getWorkingHourse(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage : " + empWage)
+console.log("Total Eployee Hrs : " + empHrs + " Total Employee Wage : " + empWage );
