9be8feaf99b52373f20e6f697147b33a1c6c2007
faizanpro77 <faizyshaikh204@gmail.com>
2021-07-04 19:31:57 +0530

    MODIFIED: add functionality add data into object

--- employeeWage.js
+++ employeeWage.js
@@ -4,8 +4,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURSE = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20; 
-const NUMBER_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 160;
+const NO_OF_WORKING_HOURS = 160;
+const NO_OF_WORKING_DAYS = 20;
 
 let empWageArray = new Array();
 //let empWageMap = new Map();
@@ -46,21 +46,14 @@ let totalEmpHrs  = 0;
 let totalWorkingDays = 0;
 
 //Calculating Wages till Number of Working Days or Total Working Hours per month is Reached
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+while (totalWorkingDays < NO_OF_WORKING_DAYS && totalEmpHrs <= NO_OF_WORKING_HOURS) {
     totalWorkingDays++
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empHrs = getWorkingHourse(empCheck);
     totalEmpHrs += empHrs;
-    // To store daily wage in array
-    empWageArray.push(calcWage(empHrs));
-
-    // Store the Day and the Daily Wage
-    empDailyWageMap.set(totalWorkingDays, calcWage(empHrs)); 
-    
-    //Store the day and employeee hourse
-    empDailyHourMap.set(totalWorkingDays, empHrs);
-
-
+    empWageArray.push(calcWage(empHrs));     // To store daily wage in array
+    empDailyWageMap.set(totalWorkingDays, calcWage(empHrs)); // Store the Day and the Daily Wage
+    empDailyHourMap.set(totalWorkingDays, empHrs);  //Store the day and employeee hourse
 }
 
 console.log(empWageArray)
@@ -149,4 +142,25 @@ console.log("Part working days : " + partWorkingDays);
 console.log("Non working days : " + nonWorkingDays);
 
 
+{
+//Ability to store the Day, Hours Worked and Wage Earned in a single object
+let totalWorkingDays = 0;
+let totalEmpHrs = 0;
 
+let empDailyHrsAndWageArr  = new Array();
+while (totalWorkingDays < NO_OF_WORKING_DAYS  && totalEmpHrs <= NO_OF_WORKING_HOURS  ) {
+    let empCheck = Math.floor((Math.random() * 10) % 3);
+    let empHrs = getWorkingHourse(empCheck);
+    totalEmpHrs += empHrs;
+    totalWorkingDays++;
+    empDailyHrsAndWageArr.push({
+        dayNum: totalWorkingDays,
+        dailyHours: empHrs,
+        dailyWage: calcWage(empHrs),
+        toString() {
+            return "\nDay : " + this.dayNum +  " Working Hourse : " + this.dailyHours + " Wage Earned : " + this.dailyWage
+        },   
+    });
+}
+console.log("Daily Hourse and Wage of employee : " + empDailyHrsAndWageArr);
+}
